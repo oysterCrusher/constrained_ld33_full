7b6f3640d726829102a167bb90b529aab6e210e0
oysterCrusher <j.adams440@gmail.com>
2015-08-24 22:03:24 +0100

    Initial commit of entire project.

---
+++ html/src/uk/me/jadams/needlefish/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package uk.me.jadams.needlefish.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import uk.me.jadams.needlefish.Needlefish;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(1280, 720);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Needlefish();
+        }
+}
